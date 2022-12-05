17f56f8c346bc3d485e51d194d3e12bc9e87d114
reebass <store1993dron@gmail.com>
2022-12-05 17:22:28 +0200

    1

--- construktor-order/construktor-order.js
+++ construktor-order/construktor-order.js
@@ -1,16 +1,50 @@
 const btnContainer = document.querySelector(".container-btn");
 const imgContainer = document.querySelector(".img")
 
+
+let paramsClientsObj = {};
+
+localStorage.clear()
+
+
 const refs = {
   btnSquare: document.querySelector('#square'),
   btnCircle: document.querySelector('#circle'),
-  btnNonStandart: document.querySelector('#non-standard')
+  btnNonStandart: document.querySelector('#non-standard'),
+  btnNextPage: document.querySelector('.next-page'),
 }
 
 console.log(btnContainer.children)
 
 
 btnContainer.addEventListener('click', onClick)
+refs.btnNextPage.addEventListener('click', onNextPage)
+
+function onNextPage(evt) {
+  evt.preventDefault();
+  // paramsClientsObj.form = document.querySelector('.active').textContent;
+
+
+  const arrBtnPageForm = btnContainer.children
+  for (let i = 0; i < arrBtnPageForm.length; i += 1) {
+    const element = arrBtnPageForm[i];
+    if(element.classList.value.includes('active')){
+      // console.log(element.textContent)
+      paramsClientsObj.form = element.textContent
+      localStorage.setItem('newItem:', JSON.stringify(paramsClientsObj))
+      // console.log(paramsClientsObj)
+    } 
+  }
+
+}
+
+
+
+function createPageStyle() {
+
+}
+
+
 
 
 function onClick(event) {
@@ -18,13 +52,9 @@ function onClick(event) {
         return;
       }
       const activeBtn = document.querySelector('.active')
-
       event.target.classList.add('active')
       activeBtn.classList.remove('active')
-    
     changeImg()
-
-      
 }
 
 
@@ -41,6 +71,4 @@ function changeImg() {
   if(refs.btnNonStandart.className.includes('active')) {
     imgContainer.src = "../image/non-standard.png"
   }
-
-
 }
